ae6f44314a39c4904b1993ac8499c82f259f80cf
Jakaria Ahmed Chowdhury <32171981+Jakaria051@users.noreply.github.com>
2018-01-30 22:40:44 +0600

    Recursive problem

---
+++ Factorial.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n)
+{
+   if(n<=1)
+    {
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+
+int main()
+{
+    int n,fact;
+    cin>>n;
+
+    fact = factorial(n);
+    cout<<fact<<endl;
+    return 0;
+
+}
